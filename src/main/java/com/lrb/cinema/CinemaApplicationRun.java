9ea6028f55d34cf30250e8e7ab746c5a188b1e81
rockmaester <rockmaester.work@gmail.com>
2023-07-25 17:44:22 +0300

    Cinema class was added. Method watchMovie was added to viever. Class Viewer was refactored.

--- src/main/java/com/lrb/cinema/CinemaApplicationRun.java
+++ src/main/java/com/lrb/cinema/CinemaApplicationRun.java
@@ -7,11 +7,11 @@ public class CinemaApplicationRun {
     public static void main(String[] args) {
         List<Viewer> viewers = new ArrayList<>();
 
-        viewers.add(new Viewer("Nick", 30, 1));
-        viewers.add(new Viewer("Sam", 32, 2));
-        viewers.add(new Viewer("Jack", 45, 4));
-        viewers.add(new Viewer("Tim", 18, 3));
-        viewers.add(new Viewer("Max", 22, 1));
+        viewers.add(new Viewer("Nick", 30));
+        viewers.add(new Viewer("Sam", 32));
+        viewers.add(new Viewer("Jack", 45));
+        viewers.add(new Viewer("Tim", 18));
+        viewers.add(new Viewer("Max", 22));
 
         System.out.println("Средний возраст зрителей: " + new ViewerStatistics().averageAge(viewers));
     }
--- src/main/java/com/lrb/cinema/Viewer.java
+++ src/main/java/com/lrb/cinema/Viewer.java
@@ -1,14 +1,18 @@
 package com.lrb.cinema;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Viewer {
     private String nickName;
     private int age;
     private int movieCount;
+    private List<Cinema> movies;
 
-    public Viewer(String nickName, int age, int movieCount) {
+    public Viewer(String nickName, int age) {
         this.nickName = nickName;
         this.age = age;
-        this.movieCount = movieCount;
+        this.movieCount = 0;
     }
 
     public String getNickName() {
@@ -33,5 +37,19 @@ public class Viewer {
 
     public void setMovieCount(int movieCount) {
         this.movieCount = movieCount;
+    }
+
+    public List<Cinema> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Cinema> movies) {
+        this.movies = movies;
+    }
+
+    public void watchMovie(Cinema movie){
+        if(this.movies == null) movies = new ArrayList<>();
+        movies.add(movie);
+        this.movieCount++;
     }
 }
